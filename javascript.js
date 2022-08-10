d307703ebb2389928d94582171664cc9f930d57c
WilliamMedina <willy14_msn@gmail.com>
2022-08-10 13:20:54 -0300

    correccion de ejercicio 9

--- javascript.js
+++ javascript.js
@@ -49,24 +49,29 @@ theList.push('ULTIMO')*/
 
 //alert("un mensaje");
 
+
 /*2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).*/
  
 //document.write("Hello World <br>");
 
+
 /*3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.*/
  
 //document.write(3 + 5);
+
 
 /*4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»*/
  
 /*let nombreDeUsuario = prompt('Ingrese su nombre de usuario');
 document.write(nombreDeUsuario);*/
 
+
 /*5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.*/
 
 /*let numero1 = parseInt(prompt('Ingrese un numero'));
 let numero2 = parseInt(prompt('Ingrese un numero'));
 document.write(numero1 + numero2);*/
+
 
 /*6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor. */
 /*
@@ -80,6 +85,7 @@ if (numerouno > numerodos){
 } else if ((numerouno !== NaN) || (numerodos !== NaN)){
     console.log("Por favor ingrese un numero")
 }*/
+
 
 /*7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.*/
 
@@ -97,6 +103,7 @@ if (primerNumero > segundoNumero && primerNumero > tercerNumero){
     console.log('Por favor ingrese un numero')
 }*/
 
+
 /*8.- Escribe un programa que pida un número y diga si es divisible por 2*/
 
 /*let numeroDivisible = parseInt(prompt('Ingrese un numero'));
@@ -107,15 +114,29 @@ if((numeroDivisible % 2) === 0){
     console.log('Su numero no es divisible por 2')
 }*/
 
+
 /*9.- Escribe un programa que pida una frase y escriba las vocales que aparecen*/
 
 /*let frase = prompt('Escriba una palabra o frase');
 
 for (var i = 0; i < frase.length; i++) {
+    if (frase[i] == "a" || frase[i] == "e" || frase[i] == "i" || frase[i] == "o" || frase[i] == "u")
     document.write(frase[i]+ "<br>")
 }*/
 
+
 /*10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)*/
+
+/*let numero = parseInt(prompt('Ingrese su numero'));
+
+if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0){
+    console.log('Su numero es divisible por 2, 3, 5 o 7')
+} else {
+    console.log('Su numero no es divisible por 2, 3, 5 o 7 o no a ingresado un numero')
+}*/
+
+
+/*11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)*/
 
 /*let numero = parseInt(prompt('Ingrese un numero'));
 
@@ -136,6 +157,4 @@ switch(0){
         console.log('Su numero no es divisible por 2, 3, 5, 7');
        break;
 }*/
-
-/*11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)*/
 
