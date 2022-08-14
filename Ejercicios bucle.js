fd7786b168ec564819bf9e0844a307e2b6645527
WilliamMedina <willy14_msn@gmail.com>
2022-08-13 23:44:08 -0300

    Ejercicios Bucle Js

--- Ejercicios bucle.js
+++ Ejercicios bucle.js
@@ -293,6 +293,7 @@ if (mayor == edad){
 
 
 /*-------- Opcion mas comoda para el usuario --------*/
+
 /*let nombreyedad = prompt('Ingrese una edad seguido de un nombre')
 let nombreyedad2 = prompt('Ingrese una edad seguido de un nombre')
 let nombreyedad3 = prompt('Ingrese una edad seguido de un nombre')
@@ -370,4 +371,22 @@ for (var i = texto.length; i > 0; i--){
 
 
 /*17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.*/
+
+/*let frase = prompt('Ingrese una frase')
+let vocales = [];
+
+for(var i = 0; i < frase.length; i++){
+    if (frase[i] == "a") {
+        vocales.push(frase.indexOf("a"));
+    } else if (frase[i] == "e") {
+        vocales.push(frase.indexOf("e"));
+    } else if (frase[i] == "i") {
+        vocales.push(frase.indexOf("i"));
+    } else if (frase[i] == "o") {
+        vocales.push(frase.indexOf("o"));
+    } else if (frase[i] == "u") {
+        vocales.push(frase.indexOf("u"));
+    }
+}
+document.write("La primera vocal se encuentra en la posicion numero " + Math.min.apply(null, vocales))*/
 
